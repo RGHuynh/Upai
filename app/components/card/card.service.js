5c0870e8988e1ea435941b8f8de7a8a268cbb55d
RGHuynh <randall.g.huynh@gmail.com>
2018-08-11 12:08:56 -0400

    style: web application

---
+++ app/components/card/card.service.js
@@ -0,0 +1,20 @@
+(function(){
+  angular
+    .module('uspaiApp')
+    .factory('cardService', cardService)
+
+    function cardService() {
+
+      var card;
+
+      var service = {
+        title: title
+      }
+
+      function getCard() {
+        return card;
+      }
+
+    }
+})()
+
